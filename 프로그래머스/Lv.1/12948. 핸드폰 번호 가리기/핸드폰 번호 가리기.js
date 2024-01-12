845aa9ce3f25877eedf9d57f77397fbd0137fdd7
parksy-13 <141797627+parksy-13@users.noreply.github.com>
2024-01-12 09:12:35 +0900

    Update 핸드폰 번호 가리기.js
    코드를 간결하게 사용했습니다.

--- 프로그래머스/Lv.1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.js
+++ 프로그래머스/Lv.1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.js
@@ -1,12 +1,5 @@
 function solution(phone_number) {
-    if(phone_number.length ===11){
-        for(let i=0;i<7;i++){
-            phone_number = phone_number.replace(phone_number[i],'*')
-        }
-    }else{
         for(let j=0;j<phone_number.length-4;j++){
             phone_number = phone_number.replace(phone_number[j],'*')
-        }
-    }
-    return phone_number
+        }return phone_number
 }
