be966a9b57e750202096083b8b8e2569b7985b23
parksy-13 <141797627+parksy-13@users.noreply.github.com>
2024-01-18 09:42:25 +0900

    [level 1] Title: 약수의 개수와 덧셈, Time: 0.13 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.js
@@ -0,0 +1,23 @@
+
+2
+3
+4
+5
+6
+7
+8
+9
+10
+11
+12
+function solution(left, right) {
+    var answer = 0;
+    for (let i = left; i <= right; i++) {
+        if (Number.isInteger(Math.sqrt(i))) {
+            answer -= i;
+        } else {
+            answer += i;
+        }
+    }
+    return answer;
+}
